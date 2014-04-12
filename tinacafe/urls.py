83bc060689d57aea5121357cb7672a6c242abe90
Heitor C <hcproradio@gmail.com>
2014-04-12 14:42:53 -0300

    update base url also

--- tinacafe/urls.py
+++ tinacafe/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^alesheets/', include('alesheets.urls')),
-    url(r'^$', 'alesheets.views.index'),
+    url(r'^$', 'alesheets.views.show_balances'),
 )
